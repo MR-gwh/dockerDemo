4d76698adc8948d17550832b688dcbaf5a33a2af
MR-G <2044783677@qq.com>
2022-01-22 18:24:36 +0800

    init project

--- src/main/java/com/example/dockerdemo/DockerDemoApplication.java
+++ src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -2,12 +2,19 @@ package com.example.dockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DockerDemoApplication.class, args);
     }
 
+    @RequestMapping("/hello")
+    public String helloDocker() {
+        return "Hello Docker";
+    }
 }
